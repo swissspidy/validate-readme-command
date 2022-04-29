925d97051941ac54edc58472656c3f09fbcb0b12
Pascal Birchler <pascalb@google.com>
2022-04-30 00:03:10 +0200

    debug

--- src/ValidateReadmeCommand.php
+++ src/ValidateReadmeCommand.php
@@ -66,6 +66,8 @@ class ValidateReadmeCommand extends WP_CLI_Command {
 			WP_CLI::error( 'No readme provided' );
 		}
 
+		var_dump( $args[0], realpath( $args[0] ), file_exists( $args[0] ), file_exists( realpath( $args[0] ) ) );
+
 		if ( realpath( $args[0] ) ) {
 			$filename       = realpath( $args[0] );
 			$readme_content = file_get_contents( $filename );
